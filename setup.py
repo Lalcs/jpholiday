a2cf1b873b4feb2e855a5aea726854f453c3bfe1
Vatis <vatis@lalcs.com>
2020-11-20 01:20:59 +0900

    mod setup

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
 		'Programming Language :: Python :: 3.6',
 		'Programming Language :: Python :: 3.7',
 		'Programming Language :: Python :: 3.8',
+		'Programming Language :: Python :: 3.9',
 	],
 	data_files=[('', ['README.md'])],
 	long_description='{}'.format((open('README.md', encoding='utf8').read())),
