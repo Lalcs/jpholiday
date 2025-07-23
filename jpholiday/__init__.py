fa8ca5a0600d2b022ca3d857a4375a9d2e3c8789
Kazuki Hayashi <k.hayashi@fhevalec.jp>
2025-07-23 22:29:34 +0900

    migrate to Poetry, adjust workflows, and refactor package configuration

--- jpholiday/__init__.py
+++ jpholiday/__init__.py
@@ -1,6 +1,22 @@
-from jpholiday.checker.interface import OriginalHolidayCheckerInterface
-from jpholiday.functions import *
-from jpholiday.jpholiday import JPHoliday
+from .checker.interface import OriginalHolidayCheckerInterface
+from .functions import *
+from .jpholiday import JPHoliday
+
+__all__ = [
+    # クラス
+    "JPHoliday",
+    "OriginalHolidayCheckerInterface",
+    # 関数
+    "is_holiday",
+    "is_holiday_name",
+    "year_holidays",
+    "month_holidays",
+    "between",
+    "register",
+    "unregister",
+    # バージョン
+    "__version__",
+]
 
 """
 日本の祝日を扱うクラス
@@ -12,4 +28,9 @@ from jpholiday.jpholiday import JPHoliday
 2020年 五輪特別措置法改正案
 """
 
-__version__ = '1.0.2'
+# Version will be dynamically set by poetry-dynamic-versioning
+try:
+    from ._version import __version__
+except ImportError:
+    # Fallback for development
+    __version__ = '0.0.0'
